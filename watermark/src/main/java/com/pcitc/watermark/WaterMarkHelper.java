bc4e818fb868c9d735e884a7e8b792fba97901bf
黄新元 <xinyuan.huang@pcitc.com>
2022-08-11 14:22:25 +0800

    增加删除水印的方法

--- watermark/src/main/java/com/pcitc/watermark/WaterMarkHelper.java
+++ watermark/src/main/java/com/pcitc/watermark/WaterMarkHelper.java
@@ -2,6 +2,7 @@ package com.pcitc.watermark;
 
 import android.app.Activity;
 import android.app.Application;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -20,14 +21,29 @@ public class WaterMarkHelper {
     /**
      * 给单独的view设置水印背景。水印会被view的内容遮挡。
      *
-     * @param view
-     * @return
+     * @param view 要添加水印的view
+     * @return 添加到view的水印背景drawable，方便后续对水印设置其他属性。
      */
     public static WaterMarkDrawable addWaterMark(@NonNull View view) {
-        String waterMarkString = "xinyuan.huang\n黄新元\n183111121222";
-        WaterMarkDrawable background = new WaterMarkDrawable(waterMarkString);
-        view.setBackground(background);
+        WaterMarkDrawable background = new WaterMarkDrawable();
+        addWaterMark(view, background);
         return background;
+    }
+
+    public static void addWaterMark(@NonNull View view, Drawable background) {
+        view.setBackground(background);
+    }
+
+    /**
+     * 移除view的水印背景
+     *
+     * @param view 要移除水印的view
+     */
+    public static void removeWaterMark(@NonNull View view) {
+        Drawable background = view.getBackground();
+        if (background instanceof WaterMarkDrawable) {
+            view.setBackground(null);
+        }
     }
 
     /**
@@ -35,24 +51,44 @@ public class WaterMarkHelper {
      * 这种方法水印会覆盖在页面的最顶层，水印会遮挡页面内容。
      * 入侵性比较小
      *
-     * @param activity
-     * @return
+     * @param activity 要添加水印的activity
+     * @return 添加到activity的水印背景drawable，方便后续对水印设置其他属性。
      */
     public static WaterMarkDrawable addWaterMark(Activity activity) {
+        WaterMarkDrawable background = new WaterMarkDrawable();
+        addWaterMark(activity, background);
+        return background;
+    }
+
+    public static void addWaterMark(Activity activity, Drawable background) {
         //找到activity的顶层布局DecorView里面的 R.id.content
         FrameLayout rootLayout = activity.findViewById(android.R.id.content);
         RelativeLayout rlWaterMark = rootLayout.findViewWithTag("rlWaterMark");
         if (rlWaterMark == null) {
             rlWaterMark = new RelativeLayout(activity);
         }
-        String waterMarkString = "xinyuan.huang\n黄新元\n183111121222";
-        WaterMarkDrawable background = new WaterMarkDrawable(waterMarkString);
         rlWaterMark.setBackground(background);
         rlWaterMark.setTag("rlWaterMark");
+        //先remove，再重新add，是为了重新绘制background。
         if (rlWaterMark.getParent() != null) {
             ((ViewGroup) rlWaterMark.getParent()).removeView(rlWaterMark);
         }
         rootLayout.addView(rlWaterMark);
-        return background;
+    }
+
+    /**
+     * 移除activity上的水印
+     *
+     * @param activity 要移除水印的activity
+     */
+    public static void removeWaterMark(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+        FrameLayout rootLayout = activity.findViewById(android.R.id.content);
+        RelativeLayout rlWaterMark = rootLayout.findViewWithTag("rlWaterMark");
+        if (rlWaterMark != null) {
+            rlWaterMark.setBackground(null);
+        }
     }
 }
